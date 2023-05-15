89fa3709b84c61a7287ed59756abde7e9aefae36
kenya0927 <123355087+kenya0927@users.noreply.github.com>
2023-05-15 08:58:18 +0000

    [Add]ルート設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
 
   has_many :books,dependent: :destroy
+  has_one_attached :profile_image
+  
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : "no_image.jpg"
+  end
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
